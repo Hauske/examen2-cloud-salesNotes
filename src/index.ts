7d6d534ffee049914b59b87e003c53b923d85083
Hauske <dany.rams@hotmail.com>
2025-11-22 18:58:28 -0600

    port fix

--- src/index.ts
+++ src/index.ts
@@ -3,7 +3,7 @@ import routes from "./routes";
 import "dotenv/config";
 
 const app = express();
-const port = process.env.PORT || 3001;
+const port = process.env.PORT || 3000;
 
 app.use(express.json());
 app.use(routes);
